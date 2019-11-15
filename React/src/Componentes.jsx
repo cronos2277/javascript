7d141eb66dc506d6a0601cefdea29c6a799e1b9c
Jotape <cronos2277@protonmail.com>
2019-11-14 21:24:17 -0300

    React

--- React/src/Componentes.jsx
+++ React/src/Componentes.jsx
@@ -1,7 +1,7 @@
 //Aqui nós temos um arquivo JSX, é a mesma coisa que o js, mas com suporte a JSX.
 import React from 'react'
 
-//o export defaul exporta uma função por padrão, lembrando que ela não precisa ser arrow.
+//o export default exporta uma função por padrão, lembrando que ela não precisa ser arrow.
 //Além disso você pode passar parametros da função usando as chaves e colocando o atributo.
 //Funciona assim: você coloca o "parametro da funcao".o atrributo que espera receber no jsx, 
 //colocando entre {} voce tem acesso a esses atributos, por exemplo:
--- React/src/MultiplosComponentes.jsx
+++ React/src/MultiplosComponentes.jsx
@@ -1,9 +1,20 @@
 //Aqui nós temos um arquivo JSX, é a mesma coisa que o js, mas com suporte a JSX.
 import React from 'react'
-
+/*
+	Aqui tem um outro exemplo, caso a função não seja padrão, você precisa
+	colocar "const, let ou var", além disso uma das formas de exportar essas
+	funções é criando um objeto, com todas as funções que você quer importar
+	dentro, lembrando que o publico aqui no caso é o que vai para o export.
+	Assim como eu enviei um objeto, você poderia enviar uma função ou qualquer
+	coisa no export.
+*/
 export const Primeiro = props => <h2>Componente não padrão, primeiro componente.</h2>
 export const Segundo = function Segundo(){
 	return <h2>Componente não padrão, segundo elemento.</h2>;
 }
-//export default Ultimo;
+
+//No caso para usar essas funções aqui, você usa como se fosse um metodo.
+//Lá no JS você tem o import, supondo que seja import Componente from './MultiplosComponentes',
+//será algo do tipo <Componente.Primeiro /> ou Componente.Segundo /> é como se o import
+//criasse um objeto no padrão singleton.
 export default {Primeiro,Segundo};
