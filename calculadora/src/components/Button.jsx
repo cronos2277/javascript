89ed64954b5d34f710ae674d9c6b8bf8d8bf613e
Jotape <cronos2277@protonmail.com>
2019-11-20 00:08:58 -0300

    calculadora

--- calculadora/src/components/Button.jsx
+++ calculadora/src/components/Button.jsx
@@ -1,4 +1,13 @@
 import React from 'react'
 import './Button.css'
+
+function isop(props){
+	let classes = "button";
+	classes += props.operation ? ' operation' : '';
+	classes += props.double ? ' double' : '';
+	classes += props.triple ? ' triple' : '';
+	return classes;
+}
 export default props => 
-	<button className='button'>{props.label}</button>
+	<button className={isop(props)}
+onClick={e=>props.click(props.label)}>{props.label}</button>
