0a39c1fa339949d2c610c3e724b9d16c14efdc9a
JP Linux Debian <cronos2277@gmail.com>
2019-11-24 21:52:03 -0300

    react crud

--- crud-react/frontend/src/main/App.jsx
+++ crud-react/frontend/src/main/App.jsx
@@ -1,13 +1,16 @@
 import './App.css';
 import React from 'react';
+import { HashRouter } from 'react-router-dom';
+import Router from './Router';
 import Logo from '../components/template/Logo';
 import Nav from '../components/template/Nav';
-import Home from '../components/home/Home';
 import Footer from '../components/template/Footer';
 export default props =>
+<HashRouter>
 <div className="app">
 	<Logo />
 	<Nav />
-	<Home />
+	<Router />
 	<Footer />
 </div>
+</HashRouter>
