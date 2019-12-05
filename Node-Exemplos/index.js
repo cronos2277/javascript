3f6f2e82ea961f52e0e4ad446d502de866527c95
JP Linux Debian <cronos2277@gmail.com>
2019-12-05 16:42:25 -0300

    express

--- Node-Exemplos/index.js
+++ Node-Exemplos/index.js
@@ -3,24 +3,41 @@
     passar em alguma variavel ou constante o resultado que ele
     retorna como funcao.
 */
-const express = require('express');
-const app = express();
+const express = require('express'); //importando.
+const app = express(); //Instanciando
+/*
+    Mais informacoes:
+    https://expressjs.com/pt-br/4x/api.html#app
+*/
 
 const funcaoCallbackSucesso = function funcaoCallbackSucesso(){
     console.log("Sucesso na conexao");
 }
 
 function callbackRequisicaoResposta(requisicao,resposta){
-    resposta.send("Estou <b>bem</b>");
+/*
+    A funcao de callback do use aceita 4 parametros
+*/
+//Esse metodo pode retornar texto plano ou html.
+resposta.send("Estou <b>bem</b>");    
+//Nada sera processado depois dessa instrucao acima, ou seja ponha o seu codigo antes
 }
 function callbackRequisicaoCustomizada(requisicao,resposta){
-    resposta.send("<b>MUDOU</b>");
+//Esse pode retornar em formato json transformando o objeto abaixo:
+    resposta.json({
+        name:"Exemplo de Nome",
+        valor:384.67,
+        verdadeiro:true
+    });
+//Nada sera processado depois dessa instrucao acima, ou seja ponha o seu codigo antes    
 }
 function callbackReqResGet(requisicao,resposta){
-    resposta.send("<b>Esta No GET</b>")
+    resposta.send("<b>Esta No GET</b>");
+//Nada sera processado depois dessa instrucao acima, ou seja ponha o seu codigo antes    
 }
 function callbackReqResPost(requisicao,resposta){
-    resposta.send("<b>Esta No Post</b>")
+    resposta.send("<b>Esta No Post</b>");
+//Nada sera processado depois dessa instrucao acima, ou seja ponha o seu codigo antes    
 }
 /*
     QUalquer requisicao a pagina caira dentro do metodo use.
