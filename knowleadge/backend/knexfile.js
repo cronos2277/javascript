e4af48508329bee3739b17e4ed64a741c47b3724
JP Linux Debian <cronos2277@gmail.com>
2019-12-10 13:59:49 -0300

    knex

--- knowleadge/backend/knexfile.js
+++ knowleadge/backend/knexfile.js
@@ -1,17 +1,21 @@
-// Aqui eh configurado as opcoes do DB. Esse eh o arquivo de configuracao do knex
+// Update with your config settings.
 
-module.exports = {  
-    client: 'postgresql',
-    connection: {
-      database: 'knowleadge',
-      user:     'postgres',
-      password: ''
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
+module.exports = {    
+        client: 'postgres',
+        connection: {
+            host : '127.0.0.1',
+            user : 'postgres',
+            password : '123456',
+            database : 'knowleadge',
+            port:'5432',
+            charset: 'utf8'
+        },
+        pool: {
+            min: 2,
+            max: 10
+        },
+        migrations: {
+            tableName: 'knex_migrations'
+        }   
+
 };
