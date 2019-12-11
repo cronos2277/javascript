cb632be2b0d3f7b9637604bbcf33c3969356ba9c
JP Windows <cronos2277@gmail.com>
2019-12-10 22:19:41 -0300

    Desafio User By ID

--- knowleadge/backend/api/user.js
+++ knowleadge/backend/api/user.js
@@ -39,7 +39,14 @@ module.exports = app =>{
         .then(users => res.json(users))
         .catch(err => res.status(500).send(err));
     }
-    return {save,get}
+    const getById = (req,res) =>{
+        app.db('users').select('id','name','email','admin')
+        .where({id: req.params.id})
+        .first()
+        .then(user => res.json(user))
+        .catch(err => res.status(500).send(err));
+    }
+    return {save,get,getById}
 }
 /*
 Aqui eh exportado o save com o nome para fora chamado save,
--- knowleadge/backend/config/routes.js
+++ knowleadge/backend/config/routes.js
@@ -12,7 +12,10 @@
     a necessidade de importar modulos.
 */
 module.exports = app =>{
-    app.route('/users').post(app.api.user.save)
+    app.route('/users')
+    .post(app.api.user.save)
     .get(app.api.user.get);
-    app.route('/users/:id').put(app.api.user.save);
+    app.route('/users/:id')
+    .put(app.api.user.save)
+    .get(app.api.user.getById);
 }
