a101227fd83e6c3eee1ad190255b1437ce522a31
JP Linux Debian <cronos2277@gmail.com>
2019-12-11 00:31:29 -0300

    explicacao2

--- knowleadge/backend/api/user.js
+++ knowleadge/backend/api/user.js
@@ -1,11 +1,36 @@
+/*
+    Esse modulo ele permite a criptografia dos dados e sera usado para criptografar a senha.
+ */
 const bcrypt = require('bcrypt-nodejs');
 module.exports = app =>{
+    /*
+        Aqui eh usado o operador destructor do atributo criado pela funcao
+        que consign(), com base no arquivo validator da pasta app. 
+     */
     const {existsOrError, notExistsOrError, equalsOrError } = app.api.validator;
     const encryptPassword = password => {
+        /*
+            Aqui a senha eh salgada, eh aqui que a criptografia acontece 
+            o 10 passado como parametro eh a quantidade de segundos que leva
+            para alterar a criptografia, por mais que a senha seja exatamente
+            a mesma, o hash sera diferente e como foi passado o parametro como
+            10, esse hash sera alterado a cada 10 segundos. Esse valor sera usado
+            como parametro para a funcao abaixo.
+         */
         const salt = bcrypt.genSaltSync(10);
+        /*
+            o metodo abaixo precisa de 2 parametros, o primeiro eh a senha que o
+            usuario informada pelo cliente, sendo o segundo parametro o valor 
+            gerado pelo metodo acima.
+         */
         return bcrypt.hashSync(password, salt);
     }
+    //Metodo assincrono.
     const save = async (req,res) =>{
+        /*
+            Operador Spread, aqui basicamente transforma em variavel
+            todas os metodos desse objeto.
+         */
         const user = {...req.body};
         if(req.params.id){
             user.id = req.params.id;            
@@ -22,19 +47,44 @@ module.exports = app =>{
             }  
 
         }catch(msg){
+            //Em caso de erro retorna o estatus 400 e a mensagem de erro.
             return res.status(400).send(msg);
         }
+        //Aqui eh chamado aquela funcao de encriptar password da funcao acima.
         user.password = encryptPassword(user.password);
+        //o delete apaga a variavel da memoria, nesse caso a Confirmação de senha, que nao vai ao BD.
         delete user.confirmPassword;
+                
         if(user.id){
+            /*
+                <parametro do consign>.<o banco de dado atribuido no index.js>('<Nome da tabela criada no knex>')
+                .update(<Objeto sendo no padrao {atributoDaTabela:valor}) //Metodo para atualizacao
+                .where({<Objeto sendo no padrao {atributoDaTabela:valor}) //Metodo para consulta, no caso aqui por ID
+                .then(_=>res.status(204).send()) //Caso de certo retorna o status 204 sem mensagem.
+                .catch(err => res.status(500).send(err)); //caso de erro retorna status 500 com a mensagem de erro.
+             */
             app.db('users').update(user).where({id: user.id})
             .then(_=>res.status(204).send()).catch(err => res.status(500).send(err));
         }else{
+            /*
+                <parametro do consign>.<o banco de dado atribuido no index.js>('<Nome da tabela criada no knex>')
+                .insert(<Objeto sendo no padrao {atributoDaTabela:valor})// Metodo para criacao, diferente do update, esse cria um novo registro.
+                .then(_=>res.status(204).send()) //Caso de certo retorna o status 204 sem mensagem.
+                .catch(err => res.status(500).send(err)); //caso de erro retorna status 500 com a mensagem de erro.
+             
+             */
             app.db('users').insert(user).then(_=>res.status(204).send())
             .catch(err => res.status(500).send(err));
         }
     }
     const get = (req,res) =>{
+        /*
+            O metodo abaixo faz a leitura no banco de dados, exemplo:
+            <metodo do consign>.<o banco de dado atribuido no index.js>('<Nome da tabela criada no knex>')
+            .select('<campo1>','<campo2>','<campo3>','<campo4>') //metodo de selecao
+            .then(users => res.json(users)) //callback de resposta para o cliente.
+            .catch(err => res.status(500).send(err)); //callback de resposta caso tenha algum erro.
+         */
         app.db('users').select('id','name','email','admin')
         .then(users => res.json(users))
         .catch(err => res.status(500).send(err));
@@ -49,8 +99,8 @@ module.exports = app =>{
     return {save,get,getById}
 }
 /*
-Aqui eh exportado o save com o nome para fora chamado save,
-seria algo do tipo: return {save: save} E essa variavel app
-tambem contem a estrutura do consign, uma vez que o mesmo eh
-carregado na inicializacao.
+    Aqui eh exportado o save com o nome para fora chamado save,
+    seria algo do tipo: return {save: save,get: get, getById: getById} E essa variavel app
+    tambem contem a estrutura do consign, uma vez que o mesmo eh
+    carregado na inicializacao.
 */
