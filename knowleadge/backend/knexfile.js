50ce0450aec9355230adacb844f8511f1ee72aa9
JP Linux Debian <cronos2277@gmail.com>
2019-12-14 17:51:03 -0300

    atualizacao

--- knowleadge/backend/knexfile.js
+++ knowleadge/backend/knexfile.js
@@ -4,6 +4,26 @@
     do module.exports, voce pode criar um novo template desse indo no terminal
     e digitando knex init no diretorio da aplicao, uma vez que o knex esteja instalado
     de maneira global.
+    Exemplo:
+    module.exports = {    
+        client: 'postgres',
+        connection: {
+            host : '127.0.0.1',
+            user : 'postgres',
+            password : '123456',
+            database : 'knowleadge',
+            port:'5432',
+            charset: 'utf8'
+        },
+        pool: {
+            min: 2,
+            max: 10
+        },
+        migrations: {
+            tableName: 'knex_migrations'
+        }   
+
+};
 */
 
 module.exports = {    
@@ -25,3 +45,4 @@ module.exports = {
         }   
 
 };
+const db = require('./.env');
