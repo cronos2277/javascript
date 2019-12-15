83fe6bfc67c6886bc38ca63eecc3e0181eab07a4
JP Linux Debian <cronos2277@gmail.com>
2019-12-15 15:38:28 -0300

    Vue Router

--- knowleadge/frontend/src/components/template/Header.vue
+++ knowleadge/frontend/src/components/template/Header.vue
@@ -7,8 +7,8 @@
       <a class="toggle" @click=toggleMenu v-if="!hideToggle">
           <i class="fa fa-lg" :class="icon"></i>          
       </a>
-      <h1 class="title">
-              {{title}}
+      <h1 class="title">              
+        <router-link to="/">{{title}}</router-link>
     </h1>
     <UserDropdown v-if="!hideUserDropDown" />
   </header>
@@ -66,6 +66,10 @@ export default {
         text-align:center;
     }
     .title a{
+        color:#fff;
+        text-decoration: none;        
+    }
+    .title a:hover{
         color:#fff;
         text-decoration: none;        
     }
--- knowleadge/frontend/src/main.js
+++ knowleadge/frontend/src/main.js
@@ -3,9 +3,13 @@ import Vue from 'vue';
 import App from './App';
 import './config/bootstrap'
 import store from './config/store'; //Importando Store.
+import router from "./config/router";
 Vue.config.productionTip = false;
 //repare que o store eh incluido aqui.
 new Vue({
-  store,
+//<- equivalente a store: store, 
+//quando o atributo tem o mesmo nome que o valor isso eh permitido.  
+  store, 
+  router: router, //<-ou pode ser na maneira classica.
   render: h => h(App)
 }).$mount('#app')
