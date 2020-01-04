044bffc10dd414b991f138e66c720728dfabd6b7
JotaPe <cronos2277@gmail.com>
2020-01-04 02:18:44 -0300

    comentarios do Global.js

--- knowleadge/frontend/src/global.js
+++ knowleadge/frontend/src/global.js
@@ -1,5 +1,13 @@
 import Vue from 'vue'
-//a userKey sera a chave.
+/*
+    a userKey sera a variavel que sera usado na aplicacao.
+    e o valor aqui sera o nome que o local storage usara
+    para criar o arquivo, esse conteudo pode ser visualizado
+    chamando no console do navegador e digitar:
+    localStorage.__knowledge_user => voce tera acesso ao dados salvos
+    em uma string json. logo o user key sera usado no Auth.vue como
+    um parametro do metodo setItem do localStorage
+*/
 export const userKey = '__knowledge_user'
 //porta que a aplicacao rodara: 4000
 export const baseApiUrl = 'http://localhost:4000' 
