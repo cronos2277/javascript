3df34d985f168c90c8860d3692ad788ca102067e
JotaPe <cronos2277@gmail.com>
2020-01-21 11:00:23 -0300

    08Classe.ts

--- #TypeScript/08Classe.ts
+++ #TypeScript/08Classe.ts
@@ -40,3 +40,35 @@ console.log(exemplo1.atributo5);
 //o valor 5 que eh correspondente ao atributo 5, eh alterado
 const exemplo1a:Exemplo1 = new Exemplo1(6);
 console.log(exemplo1a.atributo5); 
+
+class Exemplo2{
+    str1:string = "valor padrao 1";
+    str2:string = "valor padrao 2";    
+    /*
+        Aqui temos um exemplo de como escrever um metodo
+        no TypeScript. Quando o visualizador de acesso eh
+        omitido, por padrao se assume o public, alem disso
+        se faz necessario especificar um retorno.
+    */
+    metodo1():string{
+        return this.str1;
+    }
+    /*
+        Aqui temos um exemplo de como funciona a questao do
+        escopo das variaveis. Sempre que for refenciado uma
+        variavel da classe, deve-se usar o this na frente,
+        do contrario a variavel sera procurada apenas no escopo
+        local. OU seja se voce colocar uma variavel do tipo b,
+        sem o this na frente dentro de um metodo, caso a variavel
+        nao exista dentro do metodo, o mesmo nao sera procurado
+        em escopo mais amplo e dara erro, ou seja dentro de classe
+        o this do javascript funciona como o this de qualquer uma
+        linguagem orientada a objeto.
+    */
+    public metodo2(str2:string):string{
+        return "str2 da classe: "+this.str2+", str2 interno: "+str2;
+    }
+}
+const exemplo2:Exemplo2 = new Exemplo2();
+console.log(exemplo2.metodo1());
+console.log(exemplo2.metodo2('argumento passado'));
