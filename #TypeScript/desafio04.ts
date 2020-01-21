d0e3805201f903bab69ac3e60bf627c63a5e20e0
JotaPe <cronos2277@gmail.com>
2020-01-21 10:34:26 -0300

    correcao desafio 04

--- #TypeScript/desafio04.ts
+++ #TypeScript/desafio04.ts
@@ -4,4 +4,6 @@ class Produto{
     }
 }
 const produto1:Produto = new Produto("Computador",1500);
-const produto2:Produto = new Produto("Notebook",3000,20);
+console.log(produto1);
+const produto2:Produto = new Produto("Notebook",3000,0.20);
+console.log(produto2);
