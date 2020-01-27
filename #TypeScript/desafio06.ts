910e09fe42ea330a98ad25efc91d96ee18c017ed
JotaPe <cronos2277@gmail.com>
2020-01-27 10:22:06 -0300

    desafio06

---
+++ #TypeScript/desafio06.ts
@@ -0,0 +1,20 @@
+class Fila<Generico>{
+    private fila:Array<Generico> = [];
+    public entrar(elemento:Generico):void{
+        this.fila.push(elemento);        
+    }
+    public proximo():void{
+        this.fila.shift();
+    }    
+    public imprimir():void{
+        console.log(this.fila);
+    }
+    constructor(public sequencias:Array<Generico>){
+        this.fila = sequencias;
+    }
+} 
+
+const filaObj:Fila<number> = new Fila([1,2,3,4,5,6,7]);
+filaObj.entrar(8);
+filaObj.proximo();
+filaObj.imprimir();
