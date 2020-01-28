4e5ff351936c231b667c5dc6ffc89e25b8e27e32
JotaPe <cronos2277@gmail.com>
2020-01-28 00:53:29 -0300

    desafio07.ts

--- #TypeScript/desafio07.ts
+++ #TypeScript/desafio07.ts
@@ -3,7 +3,31 @@
     // Array de Objetos (Chave/Valor) -> itens
     // Métodos: obter(Chave), colocar({ C, V })
     // limpar(), imprimir()
-    
+type Map<Key,Value> = {
+    chave:Key | null,
+    valor:Value | null
+}
+
+class Mapa<Chave,Valor>{        
+    private _mapas:Map<Chave,Valor>[] = [];   
+
+    public colocar(mapa:{chave:Chave,valor:Valor}):void{
+        this._mapas.push(mapa); 
+    }
+
+    public obter(chave:number):Map<Chave,Valor> | null{
+        return this._mapas[chave];        
+    }   
+    public imprimir():void{
+        console.log(this._mapas);
+    }
+
+    public limpar():void{
+        this._mapas = [];
+    }
+}
+
+
     const mapa = new Mapa<number, string>()
     mapa.colocar({ chave: 1, valor: 'Pedro' })
     mapa.colocar({ chave: 2, valor: 'Rebeca' })
