ac0b5edbec95918ed0879345f8eb5bec4bffbe7e
JP Windows <cronos2277@gmail.com>
2020-02-01 13:31:29 -0300

    angular seletores e explicacoes

--- Aulas/Angular/myapp/src/app/mybasic.components.ts
+++ Aulas/Angular/myapp/src/app/mybasic.components.ts
@@ -8,15 +8,25 @@ import { Component } from "@angular/core";
 */
 
 @Component({
-    selector: 'mybasic', //Aqui o seletor html, a tag que sera usado, nesse caso sera <mybasic></mybasic>
-    template: `<h1>Meu Componente Basico</h1>` //Essa seria uma forma de voce colocar um html mais simples.
+    selector: 'mybasic', //Aqui o seletor html, no caso como ele sera usado, nesse caso sera <mybasic></mybasic>. Leia mais abaixo.
+    template: `<H2>Meu Componente Basico</H2>`, //Essa seria uma forma de voce colocar um html mais simples.
+    styles:[`*{color:red}`] //Aplicando estilo ao componente, detalhe, quando declarado aqui, ele tera o escopo apenas do componente.
 })
 export class MyBasicComponent{
     /* 
         A notacao do angular para a classe eh:
     O nome do componente em camelcase com o que ele eh,
-    no caso temos: "MyFirst"Component, o que esta entre
+    no caso temos: "MyBasic"Component, o que esta entre
     aspas duplas eh o nome da classe, o que esta fora
     eh a identificacao do componente no Angular.
     */
 }
+/*
+    Quando for aplicar estilo, da forma mencionada acima, lembre-se de colocar todo o css, dentro da string, que por sua vez 
+    estara dentro de um array. Os estilos dentro das strings sao carregados na ordem que foram declarados, no caso comecando
+    no indice 0 do array string. Geralmente apenas estilos mais simples sao declarados dessa forma, o que tambem vale para o HTML
+    essa ultima observacao.
+    Sobre seletores, se estiver dentro de colchetes, como nesse caso [mybasic], nos temos uma diretiva, que seria um atributo do html,
+    se tivesse um '.' por exemplo, esse seletor seria uma classe, e por ai vai seguindo os padroes css e js. Nesse caso o mybasic
+    eh uma tag html, uma vez que tag eh referenciado dessa forma.
+*/
--- Aulas/Angular/myapp/src/app/mycomposite.components.ts
+++ Aulas/Angular/myapp/src/app/mycomposite.components.ts
@@ -9,8 +9,19 @@ import { Component } from "@angular/core";
         ao qual voce coloca direto o html, aqui voce referencia
         um arquivo html.
     */
-    templateUrl: './mycomposite.components.html'
+    templateUrl: './mycomposite.components.html',
+    /*
+        Aqui eh referenciado um style externo.
+    */
+    styleUrls:['./mycomposite.components.css'],
 })
 export class MyCompositeComponent{
    
 }
+
+/*
+    Eh sempre valido lembrar que o styleUrls aceita apenas
+    um array de String, validacao feita pelo typescript inclusive,
+    comecando a carregar apartir do indice zero do array e carregando
+    na sequencia que for declarado.
+*/
