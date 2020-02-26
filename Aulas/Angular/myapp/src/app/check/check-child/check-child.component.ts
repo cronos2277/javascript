c5be5c7a0e4004e4eaa719c84f42af17b0d25571
JotaPe <cronos2277@gmail.com>
2020-02-25 23:37:08 -0300

    checks angular

--- Aulas/Angular/myapp/src/app/check/check-child/check-child.component.ts
+++ Aulas/Angular/myapp/src/app/check/check-child/check-child.component.ts
@@ -1,5 +1,15 @@
 import { Component} from '@angular/core';
 
+/*
+  Os Checks sao uma forma avancada de analisar eventos no
+  Angular, eles servem caso os eventos do angular nao
+  seja suficientes. Os checks monitoram toda e qualquer
+  alteracao, seja por mouse, teclado, url, ajax, etc...
+  Eh muito facil voce criar eventos infinitos com os checks,
+  logo eh necessario cuidado e sempre bom evitar de usa-los
+  caso haja alternativas.
+*/
+
 @Component({
   selector: 'app-check-child',
   templateUrl: './check-child.component.html',
@@ -18,7 +28,12 @@ export class CheckChildComponent{
     na url, conexao via ajax, ou qualquer interacao com o objeto
     window ou document, enfim a qualquer situacao esse metodo eh 
     disparado. Cuidado, pois pode acontecer situacoes dele ser 
-    executado infinitamente.
+    executado infinitamente. Esse metodo eh mais avancado, 
+    e o angular disponibiliza ele, caso o ngOnInit nao atenda as
+    necessidades, como por exemplo na criacao de um componente
+    avancado que envolve verificacao de url, teclado ou posicao
+    de mouse por exemplo, ou se um conteudo ajax foi concluido.
+    Ele tambem eh executado antes do: ngAfterContentInt
   */  
 
   ngDoCheck() {
@@ -28,6 +43,13 @@ export class CheckChildComponent{
     
     
   }
+  /*
+    Executado apos o ngAfterContentInt, tem a mesma funcionalidade
+    que o ngDoCheck, mas se diferencia do mesmo, uma vez que o
+    ngDoCheck eh executado ANTES do ngAfterContentInt e ESSE EVENTO
+    EH EXECUTADO DEPOIS DO: ngAfterContentInt.
+    Esse evento eh executado antes do ngAfterViewInit.
+  */
 
     ngAfterContentChecked() {
     if(this.doCheck === 1){  
@@ -35,7 +57,10 @@ export class CheckChildComponent{
     }
   }
 
-  
+  /*
+    Executado depois do ngAfterViewInit. Tem a mesma regra que os
+    dois checks acimas, porem difere de ambos na ordem de execucao.
+  */
   ngAfterViewChecked() {
     if(this.doCheck === 1){    
       console.log("     CheckChild: ngAfterViewChecked");
