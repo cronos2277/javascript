e81cc5c9bbed0e77b1c1274b6e412f24b4f482ab
JP Windows <cronos2277@gmail.com>
2020-03-04 16:38:38 -0300

    services files added

--- Aulas/Angular/myapp/src/app/module2/component3/component3.component.ts
+++ Aulas/Angular/myapp/src/app/module2/component3/component3.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Service1 } from '../service1.service';
+import { Service2 } from 'src/app/service2.service';
 
 @Component({
   selector: 'app-component3',
@@ -7,9 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Component3Component implements OnInit {
 
-  constructor() { }
+  num = 0;
+  text = ""; 
+  constructor(
+    private myService1: Service1,
+    private myService2: Service2) { }
 
   ngOnInit() {
+    this.num = this.myService1.num;
+    this.text = this.myService2.text;
   }
 
 }
