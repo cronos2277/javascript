3f741e8dd90cde8c91d9a9d9cf60c0299d44bd38
JP Windows <cronos2277@gmail.com>
2020-03-14 22:49:33 -0300

    angular

--- Projetos/Products-angular/src/app/products-table/products-table.component.ts
+++ Projetos/Products-angular/src/app/products-table/products-table.component.ts
@@ -2,8 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-product-table',
-  templateUrl: './product-table.component.html',
-  styleUrls: ['./product-table.component.css']
+  templateUrl: './products-table.component.html',
+  styleUrls: ['./products-table.component.css']
 })
 export class ProductTableComponent implements OnInit {
 
