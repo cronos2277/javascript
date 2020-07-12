2c3f9dad4566a888c3e2840c9f3b1d9b54b2c8e0
JP Windows <cronos2277@gmail.com>
2020-07-12 00:49:41 -0300

    Metodos produto atualizado

--- Projetos/Department-Products/app/src/app/product.service.ts
+++ Projetos/Department-Products/app/src/app/product.service.ts
@@ -5,6 +5,7 @@ import { Product } from './product';
 import { Observable } from 'rxjs';
 import { map, tap } from 'rxjs/operators';
 import { DepartmentService } from './department.service';
+import { Department } from './department';
 
 @Injectable({
   providedIn: 'root'
@@ -37,5 +38,41 @@ export class ProductService {
       this.loaded = true;
     }
     return this.productSubject$.asObservable();
+  }
+
+  add(prod:Product):Observable<Product>{
+    let departments = (prod.departments as Department[]).map(d => d._id);
+    return this.http.post<Product>(this.url, {...prod, departments})
+    .pipe(
+      tap((p) => {
+        this.productSubject$.getValue().push({...prod, _id:p._id})
+      })
+    );
+  }
+
+  del(prod:Product):Observable<any>{
+    return this.http.delete(`${this.url}/${prod._id}`)
+    .pipe(
+      tap(() => {
+        let products = this.productSubject$.getValue();
+        let i = products.findIndex(p => p._id === prod._id);
+        if(i>0){
+          products.splice(i,1);
+        }
+      })
+    );
+  }
+  update(prod:Product):Observable<Product>{
+    let departments = (prod.departments as Department[]).map(d => d._id);
+    return this.http.patch<Product>(`${this.url}/${prod._id}`, {...prod, departments})
+    .pipe(
+      tap(() => {
+        let products = this.productSubject$.getValue();
+        let i = products.findIndex(p => p._id === prod._id);
+        if(i>0){
+          products[i] = prod;
+        }
+      })
+    );
   }
 }
