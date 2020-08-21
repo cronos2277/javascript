516554bba7c71a2f8609c8ad922b8c7cff1027c0
JP Windows <cronos2277@gmail.com>
2020-08-20 23:21:00 -0300

    Firebase CRUD Complete

--- Aulas/Angular/advancedapp/src/app/firebase/firebase.component.ts
+++ Aulas/Angular/advancedapp/src/app/firebase/firebase.component.ts
@@ -37,13 +37,21 @@ export class FirebaseComponent implements OnInit {
     }else{
       this.add(user);
     }
+    /*
+      O reset ele limpa o formulario, sem parametros
+      ele deixa todos os campos em branco, voce pode
+      usar um objeto caso voce queira que um valor 
+      volte ao padrao, por exemplo, eu quero que o
+      id seja zero nesse caso "this.form.reset({id:0})",
+      o id vira zero e os outros campos ficam em branco.
+    */
     this.form.reset();
   }
 
   private add(user:User){
-    if(this.form.valid){ 
-      user.id = Math.round(Math.random()* 1000000);     
+    if(this.form.valid){       
       this.service.add(user);
+      
     }else{
       console.error("Error on add!");
     }
@@ -51,7 +59,7 @@ export class FirebaseComponent implements OnInit {
 
   private update(user:User){
     if(this.form.valid){
-      //Digite o codigo de atualizacao aqui
+      this.service.update(user);
       
     }else{
       console.error("Error on update!");
@@ -59,6 +67,26 @@ export class FirebaseComponent implements OnInit {
     }
   }
 
+  public delete(user:User){
+    /*
+      O Confirm eh uma caixa de dialogo que te faz uma
+      pergunta, se clicar em si retorna true e a exclusao
+      segue em frente, se clicar em cancelar ou em fechar
+      o confirm retorna falso e ai nao prossegue.
+    */
+    if(confirm(`Do you want to remove ${user.name}?`)){
+      this.service.delete(user);
+    }
+  }
   
+  public set(user:User){
+    /*
+      Voce pode colocar valores em reactive forms usando a
+      funcao setValue, no caso voce passa o objeto que contem
+      os atributos ou cria um objeto contendo atributos e valores
+      e o setValue vai colocar os valores setados aqui no formulario.
+    */
+    this.form.setValue(user);
+  }
 
 }
--- Aulas/Angular/advancedapp/src/app/firebase/service.service.ts
+++ Aulas/Angular/advancedapp/src/app/firebase/service.service.ts
@@ -9,23 +9,80 @@ import { User } from '../firebase/table.model';
 export class ServiceService {
 
   constructor(
+    /*
+      Nos o usamos para criar uma collection, ele seria uma especie de "Objeto Connection"
+      do PHP ou do Java. Com o uso do metodo .collection("NomeDoBanco") ele consegue retornar
+      um objeto que tem todas as funcionalidades para a conexao com o Firebase.
+    */
     private firestore:AngularFirestore
   ) { }
 
-  private collection:AngularFirestoreCollection<User> = this.firestore.collection("users");
+  //Definindo o nome do BD.
+  private readonly databaseName:string = "users"; //Usamos uma variavel para a criacao do nome do banco no firebase.
+  /*
+    No caso voce passa dentro mo metodo collection de um objeto AngularFirestoreCollection,
+    o nome do seu banco de dados no firebase, se existe ele usa o banco que existe, se nao
+    existe ele cria um novo. Repare que o valor retornado para esse metodo, sera usado para
+    fazer a conexao com o banco de dados, ele seria como se fosse o Statement do JDBC ou do PHP.
+    Porem diferente deles que exigem a criacao de um objeto connect, ele consegue fazer tudo isso
+    passando o nome do banco de dados como uma string
+  */
+  private collection:AngularFirestoreCollection<User> = this.firestore.collection(this.databaseName);
 
   public getProducts():Observable<User[]>{
+    console.log("AngularFirestore: ",this.firestore);
+    console.log("AngularFirestoreCollection<User>: ",this.collection);
+    /*
+      Veja so, esse metodo collection ele eh o "Statement" que criamos acima, porem seguindo o padrao
+      Observer, esse metodo retorna um Observable, no template se voce fizer a inscricao dele usando
+      o |async, toda vez que houver uma alteracao no banco de dados, o registro eh atualizado na pagina,
+      ou seja no template, o Observable retornado por esse objeto, uma vez inscrito ele atualiza a lista
+      dos registros que ele contem.
+    */
     return this.collection.valueChanges();
   }
 
-  public add(user:User):Promise<any>{
-    return this.collection.add(user)
+  public add(user:User):Promise<any>{    
+    const id = this.firestore.createId(); //Esse metodo cria o ID a ser usado
+    console.log("creating id",id);
+    user.id = id;
+    /*
+      Essa forma abaixo seria uma nova forma de se criar um registro, mas a funcao add
+      nao permite vincular o ID usado pela aplicacao na Entidade que usamos.
+      o metodo add assim como o doc().set() retorna uma promise. Se for usar o
+      metodo add, voce nao precisa criar um id como fizemos acima.
+    */
+    // return this.collection.add(user)
+    return this.collection.doc(id).set(user)   
+    /*
+      O metodo doc aceita como argumento uma string que sera o ID que voce ira usar.
+      no caso esse id foi criado pelo metodo createId do AngularFirestore, o set(Entidade)
+      eh o que faz o novo registro no firebase.
+    */ 
     .then(
       _ => alert("the user was added!")
     )
     .catch(
       _ => alert("Error on submiting the user.")
     )   
+  }
+
+  public update(user:User){
+    return this.collection.doc(user.id).set(user)
+    .then(
+        _ => alert("The user was updated!")
+    ).catch(
+      _ => alert("Error on updating the user.")
+    );
+  }
+
+  public delete(user:User){
+    return this.collection.doc(user.id)
+    .delete().then(
+      _ => alert("The user was removed!")
+    ).catch(
+      _ => alert("Error on removing the user.")
+    );
   }
  
 }
