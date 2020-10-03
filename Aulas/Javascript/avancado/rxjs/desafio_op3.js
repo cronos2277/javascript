ec276b5344bd6fd3e5326916f9fca0ef298fd9cd
JP Windows <cronos2277@gmail.com>
2020-10-03 14:05:07 -0300

    desafio_op3

--- Aulas/Javascript/avancado/rxjs/desafio_op3.js
+++ Aulas/Javascript/avancado/rxjs/desafio_op3.js
@@ -1,54 +1,65 @@
 const {from, Observable} = require('rxjs');
 
-function primeiro(){
-    return function(source){
-        return Observable.create(
-            subs => {
-                source.subscribe({
-                    next(val){
-                        subs.next(val);
-                        subs.complete();
-                    },
-                    complete(){
-                        subs.complete();
-                    },
-                    error(err){
-                        subs.error(err);
-                    }
-                });
-            }
-        );
-    }
-}
-
 function createPipeableOperator(callback){
-    return function pipeable(source){ 
+    return function(source){ 
         return Observable.create(
-            subs => {                
+            subs => {   
+                const sub = callback(subs);
                 source.subscribe({
-                    next(val){
-                        subs.next(
-                            callback()(val)
-                        )
-                    },
-                    complete(){
-                        subs.complete();
-                    },
-                    error(err){
-                        subs.error(err);
-                    }
-                });
+                    next:sub.next || (value => subs.next(value)),
+                    error:sub.error || (error => subs.error(error)),
+                    complete:sub.complete || (() => subs.complete())
+                });       
+                
             }
         );        
     }
 }
 
+function primeiro(){
+    return createPipeableOperator(subscriber => ({
+        next(valor){
+            subscriber.next(valor);
+            subscriber.complete();
+        }
+    }));
+}
+
+function ultimo(){
+    let dado;
+    return createPipeableOperator(subscriber => (        
+        {        
+            next(valor){
+                dado = valor;
+            },
+            complete(){
+                subscriber.next(dado);
+            }
+        }
+    ));
+}
+
+function nenhum(){
+    return createPipeableOperator(subscriber => (
+        {
+            next(valor){
+                subscriber.complete();
+            }
+        }
+        ));
+}
+
+function subsObservable(dados,operador){
+    from(dados).pipe(
+        operador()
+    ).subscribe(
+        console.log,
+        console.error
+    );
+}
 
 
-from([1,2,3,4,5,6,7,8,9]).pipe(
-   
-).subscribe(
-    console.log,
-    console.error
-);
+subsObservable([1,2,3,4,5],primeiro);
+subsObservable([1,2,3,4,5],nenhum); //Sera ignorado, nao ira gerar dado
+subsObservable([1,2,3,4,5],ultimo);
 
