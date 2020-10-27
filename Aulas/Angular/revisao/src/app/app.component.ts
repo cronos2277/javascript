b65def339f48f683ca1578973e227ffbed74cf23
JP Windows <cronos2277@gmail.com>
2020-10-27 17:59:12 -0300

    2wdt

--- Aulas/Angular/revisao/src/app/app.component.ts
+++ Aulas/Angular/revisao/src/app/app.component.ts
@@ -7,10 +7,15 @@ import { Component} from '@angular/core';
 })
 export class AppComponent {
 
+  public falso:boolean = false; //Variavel boolean que esconde a SPAN  
   public valor:number = 0; //Variavel definido via TS
 
-  //Funcao estruturado em TS
+    //Funcao estruturado em TS
   public aumentaValor():void{
     this.valor += 1;
   }
+
+  // A variavel do 2 way databinding no HTML
+  public cor:string = 'black'
+  
 }
--- Aulas/Angular/revisao/src/app/app.module.ts
+++ Aulas/Angular/revisao/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TagDirective } from './tag.directive';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -11,6 +12,7 @@ import { TagDirective } from './tag.directive';
     TagDirective
   ],
   imports: [
+    FormsModule,
     BrowserModule,
     AppRoutingModule
   ],
