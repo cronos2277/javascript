5af1035dceeb0d384e64557efb067ab1ac3571f7
JP Windows <cronos2277@gmail.com>
2020-10-27 19:43:09 -0300

    Pipes Angular

--- Aulas/Angular/revisao/src/app/app.component.ts
+++ Aulas/Angular/revisao/src/app/app.component.ts
@@ -9,11 +9,13 @@ export class AppComponent {
 
   public falso:boolean = false; //Variavel boolean que esconde a SPAN  
   public valor:number = 0; //Variavel definido via TS
-
+  
     //Funcao estruturado em TS
   public aumentaValor():void{
     this.valor += 1;
   }
+
+  public data:number = Date.now();
 
   // A variavel do 2 way databinding no HTML
   public cor:string = '#000000'
