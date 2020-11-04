ef49004afbf1cead0f8af0d625c37d63e40a1ba3
cronos2277 <cronos2277@gmail.com>
2020-11-04 19:40:51 -0300

    Exclusao funcionando

--- Projetos/crud/src/app/components/product/product-delete/product-delete.component.ts
+++ Projetos/crud/src/app/components/product/product-delete/product-delete.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Product } from '../product.model';
+import { ProductService } from '../product.service';
 
 @Component({
   selector: 'app-product-delete',
@@ -7,9 +10,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductDeleteComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private productService:ProductService,
+    private router:Router,
+    private param:ActivatedRoute
+  ) { }
+
+  product:Product;
 
   ngOnInit() {
+    const id = Number.parseInt(
+      this.param.snapshot.paramMap.get('id')
+    );
+    this.productService.readById(id)
+    .subscribe(
+      p => this.product = p,
+      console.error
+    )
   }
 
+  deleteProduct():void{
+    if(confirm(`Deseja realmente excluir '${this.product.name}' ?`)){
+      this.productService.delete(this.product.id)
+      .subscribe(
+        _ => this.productService.showMessage(`Produto ${this.product.name} excluído com sucesso!`),
+        console.error,
+        () => this.router.navigate(['/products'])
+      );
+    }
+  }
+
+  cancel(){
+    this.router.navigate(['/products']);
+  }
 }
