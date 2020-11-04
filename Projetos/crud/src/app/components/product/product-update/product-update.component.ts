c3f8187c09c0513b16f05b4fb67964533e0225f1
cronos2277 <cronos2277@gmail.com>
2020-11-04 19:05:25 -0300

    Componente atualizar produto funcionando

--- Projetos/crud/src/app/components/product/product-update/product-update.component.ts
+++ Projetos/crud/src/app/components/product/product-update/product-update.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Product } from '../product.model';
+import { ProductService } from '../product.service';
 
 @Component({
   selector: 'app-product-update',
@@ -7,9 +10,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductUpdateComponent implements OnInit {
 
-  constructor() { }
+  product:Product;
+  constructor(
+    private productService:ProductService,
+    private router:Router,
+    private param:ActivatedRoute
+  ) { }
 
   ngOnInit() {
+    const id:number = Number.parseInt(
+      this.param.snapshot.paramMap.get('id')
+    );
+    this.productService.readById(id).subscribe(
+      p => this.product = p,
+      console.error
+    );
   }
 
+  updateProduct():void{
+    this.productService.update(this.product)
+    .subscribe(
+      p => this.productService.showMessage(`Produto ${p.name} foi atualizado!`),
+      console.error,
+      () => this.router.navigate(['/products'])
+    );
+  }
+
+  cancel():void{
+    this.router.navigate(['/products']);
+  }
 }
