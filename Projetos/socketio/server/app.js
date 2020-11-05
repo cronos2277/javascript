e8c2ebeb53ec3fde7c682c64177b0772100a0a80
JP Windows <cronos2277@gmail.com>
2020-11-05 16:16:20 -0300

    Criando aplicacao servidora

---
+++ Projetos/socketio/server/app.js
@@ -0,0 +1,17 @@
+const settings = {
+    port:4444,
+    clientEventName:'message'
+}
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+io.on('connection', socket => socket.on(settings.clientEventName,
+    function message(msg){
+        console.log(msg);
+        io.emit('event',msg);
+    }
+));
+
+http.listen(settings.port, () => console.log(`Listening on ${settings.port}`));
