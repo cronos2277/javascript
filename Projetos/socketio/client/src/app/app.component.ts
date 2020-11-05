74f37955e0cf389bfc8c8592cc7a08ad3fd84927
JP Windows <cronos2277@gmail.com>
2020-11-05 16:01:44 -0300

    utilizando o servico

--- Projetos/socketio/client/src/app/app.component.ts
+++ Projetos/socketio/client/src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
-
+import {SocketIoService} from './socket-io.service'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'client';
+  nickname:string;
+  message:string;
+
+  constructor(
+    private socket:SocketIoService
+  ){}
+
+  ngOnInit(){
+    this.socket.messages().subscribe(
+      console.log
+    );
+  }
+
+  public send():void{
+    this.socket.send({
+      message:this.message,
+      from:this.nickname
+    });
+    this.message = '';
+  }
 }
