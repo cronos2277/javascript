d9532c36e08b89b2e9903234f7f0e2ff27d14511
JP Windows <cronos2277@gmail.com>
2020-11-05 15:52:19 -0300

    implementando o servico para comunicar  servidor

---
+++ Projetos/socketio/client/src/app/socket-io.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
+import * as socketio from 'socket.io-client';
+import { Message } from './message';
+@Injectable({
+  providedIn: 'root'
+})
+export class SocketIoService {
+
+  private readonly ip:string = '127.0.0.1';
+  private readonly tcp:number = 4444;
+  private readonly url:string =`http://${this.ip}:${this.tcp}`;
+  private socket = socketio(this.url);
+  private subjectMessages:Subject<Message> = new Subject<Message>(); 
+
+  constructor() {
+    this.socket.on('message',function(msg:Message){
+      this.subjectMessages.next(msg);
+    });
+  }
+
+  public send(msg:Message):void{
+    this.socket.emit('message',msg);
+  }
+
+  public messages():Observable<Message>{
+    return this.subjectMessages.asObservable();
+  }
+}
