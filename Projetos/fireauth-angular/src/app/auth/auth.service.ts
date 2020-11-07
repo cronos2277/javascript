a98981d890b8bbae53abed77762ff030b2a93dd9
JP Windows <cronos2277@gmail.com>
2020-11-06 21:44:00 -0300

    Autenticando com o FireAuth

---
+++ Projetos/fireauth-angular/src/app/auth/auth.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
+import { Observable,from, throwError } from 'rxjs';
+import { User } from './user';
+import {catchError, switchMap} from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private userCollection:AngularFirestoreCollection<User> = this.afs.collection('users');
+  constructor(
+    private afs:AngularFirestore,
+    private afAuth:AngularFireAuth
+  ) { }
+
+  public register(user:User):Observable<boolean>{
+    return from(this.afAuth.createUserWithEmailAndPassword(user.email,user.password))
+    .pipe(
+      switchMap(
+          (u) => {
+          return this.userCollection.doc(u.user.uid)
+            .set({...user, id:u.user.uid})
+            .then(()=>true);                    
+          }),
+      catchError(err => throwError(err))
+    )
+  }
+
+  public login(email:string, password:string):Observable<User>{
+    return from(this.afAuth.signInWithEmailAndPassword(email,password))
+    .pipe(
+      switchMap(u => {
+        return this.userCollection.doc<User>(u.user.uid).valueChanges
+      }),
+      catchError(() => throwError('Invalid credentials or not registered!'))
+    )
+  }
+
+  public logout():void{
+    this.afAuth.signOut();
+  }
+}
