ba601df6fe3e0a740159110318e4d895ebbedc16
cronos2277 <cronos2277@gmail.com>
2020-11-29 16:53:59 -0300

    aula 31

--- Projetos/taskManager-ionic/src/app/core/services/auth.service.ts
+++ Projetos/taskManager-ionic/src/app/core/services/auth.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import 'firebase/auth';
+import 'firebase/firestore';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private afAuth:AngularFireAuth) {
+    
+  }
+
+  private signInWithEmail({email,password}):Promise<any>{
+    return this.afAuth.signInWithEmailAndPassword(email,password);    
+  }
+
+  private signUpWithEmail({email,password, name, photoURL = null}):Promise<any>{
+    return this.afAuth.createUserWithEmailAndPassword(email,password)
+    .then(credentials => credentials.user.updateProfile(
+          {displayName: name, photoURL:photoURL}
+      ).then(() => credentials)
+    )
+    
+  }
 }
