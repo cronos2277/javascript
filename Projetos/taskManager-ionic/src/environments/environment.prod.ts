0efa6c39e33fb07e4721f33acca25e3b8e734b79
JP Windows <cronos2277@gmail.com>
2020-11-29 13:47:55 -0300

    Criando AuthService com Ionic CLI

--- Projetos/taskManager-ionic/src/environments/environment.prod.ts
+++ Projetos/taskManager-ionic/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyA96_UhAJ52SgrQzQPA-A0rcNKdeWLiyxY",
+    authDomain: "ionic-firestore-tasks-ecfa1.firebaseapp.com",
+    databaseURL: "https://ionic-firestore-tasks-ecfa1.firebaseio.com",
+    projectId: "ionic-firestore-tasks-ecfa1",
+    storageBucket: "ionic-firestore-tasks-ecfa1.appspot.com",
+    messagingSenderId: "394286206615",
+    appId: "1:394286206615:web:5379cef00aefe6283b63d2",
+    measurementId: "G-CLTB2E27CE"
+  }
 };
--- Projetos/taskManager-ionic/src/environments/environment.ts
+++ Projetos/taskManager-ionic/src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyA96_UhAJ52SgrQzQPA-A0rcNKdeWLiyxY",
+    authDomain: "ionic-firestore-tasks-ecfa1.firebaseapp.com",
+    databaseURL: "https://ionic-firestore-tasks-ecfa1.firebaseio.com",
+    projectId: "ionic-firestore-tasks-ecfa1",
+    storageBucket: "ionic-firestore-tasks-ecfa1.appspot.com",
+    messagingSenderId: "394286206615",
+    appId: "1:394286206615:web:5379cef00aefe6283b63d2",
+    measurementId: "G-CLTB2E27CE"
+  }
 };
 
 /*
