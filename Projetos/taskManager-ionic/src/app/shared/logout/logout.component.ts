7c57434ccc80e979100a99c73608e2fddb347409
JP Windows <cronos2277@gmail.com>
2020-12-03 18:07:19 -0300

    Arrumando menu

--- Projetos/taskManager-ionic/src/app/shared/logout/logout.component.ts
+++ Projetos/taskManager-ionic/src/app/shared/logout/logout.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { NavController } from '@ionic/angular';
+import { Component, Input, OnInit } from '@angular/core';
+import { MenuController, NavController } from '@ionic/angular';
 import { AuthService } from 'src/app/core/services/auth.service';
 import { OverlayService } from 'src/app/core/services/overlay.service';
 
@@ -10,13 +10,19 @@ import { OverlayService } from 'src/app/core/services/overlay.service';
 })
 export class LogoutComponent implements OnInit {
 
+  @Input() menu:string;
   constructor(
     private authServie:AuthService,
+    private menuCtrl:MenuController,
     private navCtrl:NavController,
     private overlayService:OverlayService
   ) { }
 
-  ngOnInit() {}
+  public async ngOnInit():Promise<void> {
+    if(!(await this.menuCtrl.isEnabled())){
+      this.menuCtrl.enable(true,this.menu);
+    }
+  }
 
   async logout():Promise<void>{
     await this.overlayService.alert({
@@ -26,6 +32,7 @@ export class LogoutComponent implements OnInit {
           text:'yes',
           handler: async () => {
               await this.authServie.logout();
+              await this.menuCtrl.enable(false, this.menu);
               this.navCtrl.navigateRoot('/login');
           }
         },
