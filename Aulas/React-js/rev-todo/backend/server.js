f4e70f461729a3a9819569078fad9ea683c81d04
JP Windows <cronos2277@gmail.com>
2021-02-12 01:39:05 -0300

    cors

--- Aulas/React-js/rev-todo/backend/server.js
+++ Aulas/React-js/rev-todo/backend/server.js
@@ -5,5 +5,13 @@ const server = express();
 
 server.use(bodyParser.urlencoded({extended:true}));
 server.use(bodyParser.json());
+server.use(
+    function(req,res, next){
+        res.header('Access-Control-Allow-Origin','*');
+        res.header('Access-Control-Allow-Methods','GET, POST, PUT, PATCH, DELETE');
+        res.header('Access-Control-Allow-Headers','Origin, X-Requested-With, Content-Type, Accept');
+        next();
+    }
+);
 server.listen(port,() => console.log(`Backend is running on port ${port}`));
 module.exports = server;
