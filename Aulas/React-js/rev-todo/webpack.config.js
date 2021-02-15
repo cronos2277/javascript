03fa52202444a3d9ab0fc823f02296ce79950158
JP Windows <cronos2277@gmail.com>
2021-02-15 15:47:17 -0300

    webpack fixed

--- Aulas/React-js/rev-todo/webpack.config.js
+++ Aulas/React-js/rev-todo/webpack.config.js
@@ -1,41 +1,40 @@
-const webpack = require('webpack');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-
+const webpack = require('webpack')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 module.exports = {
     entry: './src/index.jsx',
-    output:{
-        path: __dirname+'/public',
-        filename:'./app.js'
+    output: {
+        path: __dirname + '/public',
+        filename: './app.js'
     },
-    devServer:{
-        port:8080,
-        contentBase:'./public'
+    devServer: {
+        port: 8080,
+        contentBase: './public',
     },
-    resolve:{
-        extensions:['.js','.jsx'],
-        alias:{
-            modules: __dirname+'/node_modules'
+    resolve: {
+        extensions: ['', '.js', '.jsx'],
+        alias: {
+            modules: __dirname + '/node_modules'
         }
     },
-    plugins:[
+    plugins: [ 
         new ExtractTextPlugin('app.css')
     ],
-    module:{
-        rules:[{
+    module: {
+        loaders: [{
             test: /.js[x]?$/,
-            loader:'babel-loader',
-            exclude:'/node_modules/',
-            query:{
-                presets:['es2015','react'],
-                plugins:['transform-object-rest-spread']
+            loader: 'babel-loader',
+            exclude: /node_modules/,
+            query: {
+                presets: ['es2015', 'react'],
+                plugins: ['transform-object-rest-spread']
             }
-        },{
+        }, {
             test: /\.css$/,
-            loader: ExtractTextPlugin.extract('style-loader','css-loader')
-        },{
+            loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
+        }, {
             test: /\.woff|.woff2|.ttf|.eot|.svg*.*$/,
-            loader:'file'
+            loader: 'file'
         }]
     }
 }
