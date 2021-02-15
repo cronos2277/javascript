66e97cae7de706936bc5f9a05ede2b1aea31231e
JP Windows <cronos2277@gmail.com>
2021-02-15 16:40:03 -0300

    Componente page header

--- Aulas/React-js/rev-todo/src/about/about.jsx
+++ Aulas/React-js/rev-todo/src/about/about.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
+import PageHeader from '../template/pageHeader';
+
 export default props => (
     <div>
-        <h1>Sobre</h1>
+        <PageHeader name='Sobre' small='Nós'></PageHeader>
+        <h2>Nossa História</h2>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+        <h2>Missão e Visão</h2>
+        <p>Sit asperiores aut reiciendis! Cupiditate numquam sed, accusantium quis eligendi rem totam! Vel quo doloribus quas fuga est. Velit earum atque nihil.</p>
+        <h2>Imprensa</h2>
+        <p>Facere culpa obcaecati, perferendis placeat dolorem assumenda? Quas pariatur eveniet suscipit. Hic quasi aut eveniet, quisquam qui at atque nulla natus sint.</p>
     </div>
 );
