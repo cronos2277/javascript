eca45de54456cb5036401e8a19dc6ccee8aba399
JP Windows <cronos2277@gmail.com>
2021-04-01 23:42:18 -0300

    Interface grafica #3

--- Projetos/electron-legendas/src/components/Home.vue
+++ Projetos/electron-legendas/src/components/Home.vue
@@ -1,5 +1,19 @@
 <template>
     <v-container fluid>
+        <v-form>
+            <v-file-input 
+                multiple 
+                chips 
+                v-model="files"
+                label="Selecione as Legendas"
+                prepend-icon="mdi-message-text"
+                append-outer-icon="mdi-send"
+                outlined
+                @click:append-outer="processSubtitles"
+                />
+
+            
+        </v-form>
         <div class="pills">
             <Pill v-for="word in groupedWords" :key="word.name" 
                 :name="word.name" :amount="word.amount"
@@ -14,11 +28,17 @@ export default {
     components:{Pill},
     data:function(){
         return {
+            files: [],
             groupedWords:[
                 {name:"I", amount:1234},
                 {name: "you", amount:900},
                 {name:"he", amount:853}
             ]
+        }
+    },
+    methods:{
+        processSubtitles(){
+            console.log(this.files);
         }
     }
 }
